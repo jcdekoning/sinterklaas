576cb7fa5c3a70031bb43542fcb85b8a62cadbc0
Johan de Koning <johan.de.koning@if.no>
2019-09-09 21:05:10 +0200

    Client validation

--- registratie/src/App.tsx
+++ registratie/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useForm from "react-hook-form";
 
 const App = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, errors, handleSubmit } = useForm();
   const onSubmit = (data: any, e: any) => {
     console.log(data);
     console.log(e);
@@ -15,11 +15,21 @@ const App = () => {
     <fieldset>
       <label htmlFor="naam">Uw naam</label>
       <p>Vul a.u.b. uw voornaam en achternaam in.</p>
-      <input name="naam" ref={register} />
+      <input name="naam" ref={register({
+        required: "Vul uw naam in"
+      })} />
+      {errors.naam && <p className="error">{errors.naam.message}</p>}
     </fieldset>
     <fieldset>
       <label htmlFor="email">Uw emailadres</label>
-      <input name="email" ref={register} />
+      <input name="email" ref={register({
+        required: "Vul uw emailadres in",
+        pattern: {
+          value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+          message: "Het formaat van het ingevulde emailadres is ongeldig"
+        }
+      })} />
+      {errors.email && <p className="error">{errors.email.message}</p>}
     </fieldset>
     <fieldset>
       <label htmlFor="aantalvolwassenen">Met hoeveel personen (buiten de kinderen) komt u? </label>
@@ -57,6 +67,7 @@ const App = () => {
         value="Geen"
         ref={register({ required: true })}
       />Ik heb geen kind op de NTC Het Noorderlicht en ben ook niet lid van de Nederlandse Club Oslo. Ik wil wel graag lid worden van de Nederlandse Club Oslo (aanmelding kost 150 Nok per kind + 175 Nok voor het lidmaatschap tot eind 2019)</label>
+      {errors.relatie && <p className="error">Maak een keuze</p>}
     </fieldset>
     <h2>Aanmelding kinderen</h2>
     <fieldset>
@@ -72,15 +83,32 @@ const App = () => {
     <h2>Gegevens kind</h2>
     <fieldset>
       <label htmlFor="roepnaamkind">Roepnaam kind</label>
-      <input name="roepnaamkind" ref={register} />
+      <input name="roepnaamkind" ref={register({
+        required: "Vul de roepnaam van uw kind in"
+      })} />
+      {errors.roepnaamkind && <p className="error">{errors.roepnaamkind.message}</p>}
     </fieldset>
     <fieldset>
       <label htmlFor="achternaamkind">Achternaam kind</label>
-      <input name="achternaamkind" ref={register} />
+      <input name="achternaamkind" ref={register({
+        required: "Vul de achternaam van uw kind in"
+      })} />
+      {errors.achternaamkind && <p className="error">{errors.achternaamkind.message}</p>}
     </fieldset>
     <fieldset>
       <label htmlFor="leeftijdkind">Leeftijd kind</label>
-      <input name="leeftijdkind" ref={register} />
+      <input type="number" name="leeftijdkind" ref={register({
+        required: 'Vul de leeftijd van uw kind in',
+        min: {
+          value: 0,
+          message: 'Leeftijd kan niet negatief zijn'
+        },
+        max: {
+          value: 10,
+          message: 'De maximale leeftijd is 10 jaar'
+        }
+      })} />
+      {errors.leeftijdkind && <p className="error">{errors.leeftijdkind.message}</p>}
     </fieldset>
     <fieldset>
       <legend>Geslacht kind</legend>
@@ -96,10 +124,12 @@ const App = () => {
         value="Meisje"
         ref={register({ required: true })}
       />Meisje</label>
+      {errors.geslachtkind && <p className="error">Kies een geslacht</p>}
     </fieldset>
     <fieldset>
-      <label htmlFor="anekdotekind">Roepnaam kind</label>
-      <textarea name="anekdotekind" rows={3} />
+      <label htmlFor="anekdotekind">Anekdote kind</label>
+      <textarea name="anekdotekind" rows={3} ref={register} />
+      {errors.anekdotekind && <p className="error">Vul een anekdote in</p>}
     </fieldset>
     <h2>Vrijwilligers</h2>
     <p>We hebben uw hulp hard nodig op zaterdag 24 november, want zonder vrijwilligers is het onmogelijk om het Sinterklaasfeest te organiseren. We hopen dat we op uw inzet kunnen rekenen!
@@ -137,10 +167,12 @@ Let op! Bij het inplannen van de vrijwilligers wordt rekening gehouden met de au
         value="nee"
         ref={register({ required: true })}
       />Nee</label>
+      {errors.vrijwilliger && <p className="error">Maak een keuze</p>}
     </fieldset>
     <fieldset>
       <label htmlFor="commentaar">Heeft u nog overige vragen en/of opmerkingen?</label>
-      <textarea name="commentaar" rows={3} />
+      <textarea name="commentaar" rows={3} ref={register} />
+      {errors.commentaar && <p className="error">{errors.commentaar.message}</p>}
     </fieldset>
     <fieldset>
       <legend>Privacyverklaring</legend>
@@ -153,6 +185,7 @@ Let op! Bij het inplannen van de vrijwilligers wordt rekening gehouden met de au
         value="akkoord"
         ref={register({ required: true })}
       />Ik heb de privacyverklaring gelezen</label>
+      {errors.privacyverklaring && <p className="error">Accepteer de privacyverklaring</p>}
     </fieldset>
     <input type="submit" value="Naar betaling" />
   </form>
