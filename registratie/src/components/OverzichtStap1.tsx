e0110506f140be0a6121dbfe65b0479c72a939ad
Johan de Koning <github@johandekoning.nl>
2022-10-19 21:11:15 +0200

    Tekst aanpassingen voor 2022

--- registratie/src/components/OverzichtStap1.tsx
+++ registratie/src/components/OverzichtStap1.tsx
@@ -28,18 +28,18 @@ const OverzichtStap1 = (props: Stap1FormData) => {
         <li>{props.aantalPersonen}</li>
       </ul>
       {((!kindOpSchoolAsBool && !lidVanClubAsBool) || gratisLidmaatschapAsBool) && <>
-      <p>{!kindOpSchoolAsBool && !lidVanClubAsBool ?
-        "Omdat u geen kinderen heeft op de NTC Het Noorderlicht is een lidmaatschap een vereiste om deel te nemen aan het Sinterklaasfeest." :
-        "U wilt gebruik maken van het van het gratis lidmaatschap tot eind 2021. Daarna wordt het lidmaatschap automatisch verlengd."}</p>
-      <ul>
-      <li className={styles.label}>{text.stap1.straatnaam.label}</li>
-        <li>{props.straatnaam}</li>
-        <li className={styles.label}>{text.stap1.postcode.label}</li>
-        <li>{props.postcode}</li>
-        <li className={styles.label}>{text.stap1.plaats.label}</li>
-        <li>{props.plaats}</li>
-      </ul>
-    </>}
+        <p>{!kindOpSchoolAsBool && !lidVanClubAsBool ?
+          "Omdat u geen kinderen heeft op de NTC Het Noorderlicht is een lidmaatschap een vereiste om deel te nemen aan het Sinterklaasfeest." :
+          "U wilt gebruik maken van het van het gratis lidmaatschap tot eind 2022. Daarna wordt het lidmaatschap automatisch verlengd."}</p>
+        <ul>
+          <li className={styles.label}>{text.stap1.straatnaam.label}</li>
+          <li>{props.straatnaam}</li>
+          <li className={styles.label}>{text.stap1.postcode.label}</li>
+          <li>{props.postcode}</li>
+          <li className={styles.label}>{text.stap1.plaats.label}</li>
+          <li>{props.plaats}</li>
+        </ul>
+      </>}
     </div>
   );
 };
